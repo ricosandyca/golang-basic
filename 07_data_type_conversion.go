738a8ebce413dc6a6989bb6212091dd8b83c77d7
Rico Sandyca <ricosandyca.if@gmail.com>
2021-02-24 17:41:37 +0700

    Add data type conversion module

---
+++ 07_data_type_conversion.go
@@ -0,0 +1,17 @@
+/**
+ * TODO: invalid value of integer conversion
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Rico"
+	var r = name[0]
+	var rString = string(r)
+
+	fmt.Println("Name", name)
+	fmt.Println("R in Byte", r)
+	fmt.Println("R in String", rString)
+}
