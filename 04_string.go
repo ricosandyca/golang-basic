de49694411e9bd17b5926a17d7d85268915bf041
Rico Sandyca <ricosandyca.if@gmail.com>
2021-02-24 17:40:49 +0700

    Add string module

---
+++ 04_string.go
@@ -0,0 +1,18 @@
+/**
+ * String data type
+ * - string
+ *
+ * * Function For String
+ * - len("string"): count the number of chars (6)
+ * - "string"[number]: get a char in byte from the string ("string"[2] => 82)
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rico")
+	fmt.Println(len("Rico"))
+	fmt.Println("Rico"[2])
+}
