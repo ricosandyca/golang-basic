06b4707bd43073d15d684991310824ffc0cf677c
Rico Sandyca <ricosandyca.if@gmail.com>
2021-02-26 12:26:43 +0700

    Add comparison operation module

---
+++ 10_comparison_operation.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("12 > 13", 12 > 13)
+	fmt.Println("a == a", "a" == "a")
+	fmt.Println("a != b", "a" != "b")
+	fmt.Println("a != a", "a" != "a")
+	fmt.Println("1 >= 1", 1 >= 1)
+}
